f462cf8c0640ce24f7b498bc537d664643b24054
tombrennanjr <77296578+tombrennanjr@users.noreply.github.com>
2026-01-02 14:38:05 -0700

    Update armsv3.ino
    killswitch on digital pin 4 added, need to factor it out as a listener for better response during motion

--- robots/l298pwm/armsv3.ino
+++ robots/l298pwm/armsv3.ino
@@ -1,31 +1,30 @@
+/* Two motor DC pwm control on an adafruit v3 motor shield
+    kill switch added, need to get object sensors and wifi/bte */
+
+
 int directionPinA = 12;
-
 int pwmPinA = 3;
-
 int brakePinA = 9;
 
-
 //uncomment if using channel B, and remove above definitions
-
 int directionPinB = 13;
-
 int pwmPinB = 11;
-
 int brakePinB = 8;
 
-
 //boolean to switch direction
-
 bool directionStateA;
 bool directionStateB;
 
+//for the kill switch wired to d4
+const int buttonPin = 4;
+int buttonState = 0;
+int armedState = 0;
 
 void setup() {
-
-  
-
+Serial.begin(9600);
 //define pins
-
+pinMode(buttonPin, INPUT);
+  
 pinMode(directionPinA, OUTPUT);
 pinMode(directionPinB, OUTPUT);
 pinMode(pwmPinA, OUTPUT);
@@ -37,6 +36,26 @@ pinMode(brakePinB, OUTPUT);
 
 
 void loop() {
+// get arm switch status
+  Serial.println(digitalRead(buttonPin));
+  buttonState = digitalRead(buttonPin);
+
+  // read state and swap if button was pressed, arm or disarm depending...
+  if (buttonState ==1){
+    if (armedState == 0) {
+      armedState =1;
+      Serial.println("Armed");
+    }
+    else if (armedState == 1) {
+      armedState=0;
+      Serial.println("DisArmed");
+      delay(50000); 
+    }
+  }
+  else if (buttonState==LOW) delay(50);
+
+if(armedState==1)
+{
 //change direction every loop()
 
 directionStateA = !directionStateA;
@@ -62,7 +81,7 @@ analogWrite(pwmPinA, 170);
 analogWrite(pwmPinB, 170);
 
 //move
-delay(6000);
+delay(60);
 
 //activate brakes
 digitalWrite(brakePinA, HIGH);
@@ -72,7 +91,7 @@ digitalWrite(brakePinB, HIGH);
 analogWrite(pwmPinA, 0);
 analogWrite(pwmPinB, 0);
 
-delay(2000);
+delay(20);
 
 // turn in a circle ?
 //directionStateA = !directionStateA;
@@ -97,7 +116,7 @@ analogWrite(pwmPinA, 170);
 analogWrite(pwmPinB, 170);
 
 //loop time
-delay(6700);
+delay(600);
 
 //activate brakes
 digitalWrite(brakePinA, HIGH);
@@ -107,5 +126,5 @@ digitalWrite(brakePinB, HIGH);
 analogWrite(pwmPinA, 0);
 analogWrite(pwmPinB, 0);
 delay(2000);
-
+}
 }
