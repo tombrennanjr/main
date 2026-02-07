be322174ca83f920941d8f12d7ad9e319e2b3d44
tombrennanjr <77296578+tombrennanjr@users.noreply.github.com>
2026-02-06 17:52:45 -0700

    Create 4mtrsIrIntrrpt.ino
    added an interrupt handler with a volatile flag to see if we can get pin 2 firing interrupts.

---
+++ robots/l293dshield/4mtrsIrIntrrpt.ino
@@ -0,0 +1,206 @@
+ // AFMotor R4 Compatible Library - Basic Motor Test
+// Hardware Setup: DC Motor connected to M1
+
+
+#include "AFMotor_R4.h"
+
+AF_DCMotor motor1(1);  // Create motor on M1
+AF_DCMotor motor2(2);  // Create motor on M1
+AF_DCMotor motor3(3);  // Create motor on M1
+AF_DCMotor motor4(4);  // Create motor on M1
+
+const byte IRpin=2;
+volatile bool obstacleDetected = false;
+
+void setup() {
+  Serial.begin(9600);   
+
+  pinMode(IRpin,INPUT_PULLUP);
+          
+  Serial.println("Basic Motor Test!");
+
+  // Set initial speed and release motor
+  motor1.setSpeed(200);
+  motor1.run(RELEASE);
+  motor2.setSpeed(200);
+  motor2.run(RELEASE);
+  motor3.setSpeed(200);
+  motor3.run(RELEASE);
+  motor4.setSpeed(200);
+  motor4.run(RELEASE);
+
+//IR sensor
+
+  // Attach the interrupt to the pin.
+  // digitalPinToInterrupt(interruptPin) gets the correct interrupt number for the pin.
+  // The 'FALLING' mode triggers the ISR when the pin goes from HIGH to LOW (common for
+  // obstacle sensors when an object is present).
+  attachInterrupt(digitalPinToInterrupt(IRpin), obstacleStop, FALLING);
+
+// attachInterrupt(2, obstacleStop, HIGH);
+
+}
+
+void loop() {
+  uint8_t i;
+
+if(obstacleDetected){
+
+  Serial.println("Stop Stop Stop");
+  obstacleDetected=false;
+  delay(50000);
+}
+  
+  Serial.println("Running forward...");
+  
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+  for (i = 0; i < 255; i++) {
+    motor1.setSpeed(i);
+    motor2.setSpeed(i);
+    motor3.setSpeed(i);
+    motor4.setSpeed(i);
+  
+    delay(10);
+  }
+ 
+  for (i = 255; i != 0; i--) {
+    motor1.setSpeed(i);
+    motor2.setSpeed(i);
+    motor3.setSpeed(i);
+    motor4.setSpeed(i); 
+    delay(10);
+  }
+
+Serial.println("Braking...");
+  motor1.run(BRAKE);
+  motor2.run(BRAKE);
+  motor3.run(BRAKE);
+  motor4.run(BRAKE);
+  delay(100);
+  
+  Serial.println("Releasing...");
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  delay(100);
+ 
+Serial.println("Running in circles...");
+  
+  motor1.run(BACKWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(BACKWARD);
+  for (i = 0; i < 255; i++) {
+    motor1.setSpeed(i);
+    motor2.setSpeed(i);
+    motor3.setSpeed(i);
+    motor4.setSpeed(i);
+  
+    delay(10);
+  }
+ 
+  for (i = 255; i != 0; i--) {
+    motor1.setSpeed(i);
+    motor2.setSpeed(i);
+    motor3.setSpeed(i);
+    motor4.setSpeed(i); 
+    delay(10);
+  }
+
+ Serial.println("Braking...");
+  motor1.run(BRAKE);
+  motor2.run(BRAKE);
+  motor3.run(BRAKE);
+  motor4.run(BRAKE);
+  delay(100);
+  
+  Serial.println("Releasing...");
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  delay(100);
+  
+  Serial.println("Running backward...");
+  
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD); 
+  
+  for (i = 0; i < 255; i++) {
+    motor1.setSpeed(i);
+    motor2.setSpeed(i);
+    motor3.setSpeed(i);
+    motor4.setSpeed(i);  
+    delay(10);
+  }
+ 
+  for (i = 255; i != 0; i--) {
+    motor1.setSpeed(i);
+    motor2.setSpeed(i);
+    motor3.setSpeed(i);
+    motor4.setSpeed(i);  
+    delay(10);
+  }
+
+Serial.println("Braking...");
+  motor1.run(BRAKE);
+  motor2.run(BRAKE);
+  motor3.run(BRAKE);
+  motor4.run(BRAKE);
+  delay(100);
+  
+  Serial.println("Releasing...");
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  delay(100);
+  
+  Serial.println("Running in circles...");
+  
+  motor1.run(FORWARD);
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD);
+  motor4.run(FORWARD);
+  for (i = 0; i < 255; i++) {
+    motor1.setSpeed(i);
+    motor2.setSpeed(i);
+    motor3.setSpeed(i);
+    motor4.setSpeed(i);
+  
+    delay(10);
+  }
+ 
+  for (i = 255; i != 0; i--) {
+    motor1.setSpeed(i);
+    motor2.setSpeed(i);
+    motor3.setSpeed(i);
+    motor4.setSpeed(i); 
+    delay(10);
+  }
+ 
+  Serial.println("Braking...");
+  motor1.run(BRAKE);
+  motor2.run(BRAKE);
+  motor3.run(BRAKE);
+  motor4.run(BRAKE);
+  delay(1000);
+  
+  Serial.println("Releasing...");
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
+  delay(1000);
+}
+
+void obstacleStop(void)
+{
+  obstacleDetected=true;
+}
